ed01ae8942c90ca57d02e1194f801dd207652bbd
rk <t26744021@gmail.com>
2024-07-13 15:37:44 +0800

    d
    dd

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rkmcprotocol",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # 在这里列出您的依赖项，例如:
+        # 'requests',
+        # 'numpy',
+    ],
+)
